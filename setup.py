580f36c90f7f24b1f42d5bb1174423c50b5453d6
speedcell4 <speedcell4@gmail.com>
2018-08-20 16:28:43 +0900

    fix(setup.py): fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', mode='r', encoding='utf-8') as fd:
 
 setup(
     name='torch-recurrent',
-    version='0.1.1',
+    version='0.1.2',
     packages=['torch_recurrent'],
     install_requires=['torch'],
     url='https://github.com/speedcell4/torch-recurrent',
